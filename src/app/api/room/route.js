a47436c56dcc2e633c494027c1644630a7a0a085
Your Name <you@example.com>
2023-09-14 15:05:15 +0700

    ส่งงาน

--- src/app/api/room/route.js
+++ src/app/api/room/route.js
@@ -1,38 +1,53 @@
 import { DB, readDB, writeDB } from "@/app/libs/DB";
+import { headers } from "next/headers";
 import { checkToken } from "@/app/libs/checkToken";
 import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
+  console.log.headers
   readDB();
-  return NextResponse.json({
+  return NextResponse.json(
+    {
     ok: true,
-    //rooms:
-    //totalRooms:
+    rooms:DB.rooms
+    // totalRooms:
   });
-};
+}
 
-export const POST = async (request) => {
-  const payload = checkToken();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
-
-  readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room ${"replace this with room name"} already exists`,
-  //   },
-  //   { status: 400 }
-  // );
-
+  export const POST = async (request) => {
+    const payload = checkToken();
+    if(!payload){
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+    }
+    readDB();
+    const foundRoom = DB.rooms.find((x)=>x.roomId === roomId && x.roomName === roomName)
+    if(foundRoom){
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room ${"replace this with room name"} already exists`,
+      },
+      { status: 400 }
+    );
+    }
+  
+    DB.enrollments.push({
+      studentId,
+      courseNo,
+    });
+    if(!foundRoom)
+    return NextResponse.json({
+      ok: true,
+      message: "You has enrolled a course successfully",
+    });
+  };
   const roomId = nanoid();
 
   //call writeDB after modifying Database
@@ -43,4 +58,3 @@ export const POST = async (request) => {
     //roomId,
     message: `Room ${"replace this with room name"} has been created`,
   });
-};
