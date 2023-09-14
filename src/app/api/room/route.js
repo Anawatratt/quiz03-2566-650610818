077fa7ead79556666fcc5171fe301b64366413b5
Anawatratt <110977191+Anawatratt@users.noreply.github.com>
2023-09-14 15:15:07 +0700

    Update route.js

--- src/app/api/room/route.js
+++ src/app/api/room/route.js
@@ -1,23 +1,20 @@
 import { DB, readDB, writeDB } from "@/app/libs/DB";
-import { headers } from "next/headers";
 import { checkToken } from "@/app/libs/checkToken";
 import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  console.log.headers
   readDB();
-  return NextResponse.json(
-    {
+  return NextResponse.json({
     ok: true,
-    rooms:DB.rooms
-    // totalRooms:
+    rooms: DB.rooms,
+    totalRooms: DB.rooms.length,
   });
-}
+};
 
-  export const POST = async (request) => {
-    const payload = checkToken();
-    if(!payload){
+export const POST = async (request) => {
+  const payload = checkToken();
+  if (!payload || (payload.role !== "ADMIN" && payload.role !== "SUPER_ADMIN")) {
     return NextResponse.json(
       {
         ok: false,
@@ -25,36 +22,34 @@ export const GET = async () => {
       },
       { status: 401 }
     );
-    }
-    readDB();
-    const foundRoom = DB.rooms.find((x)=>x.roomId === roomId && x.roomName === roomName)
-    if(foundRoom){
+  }
+
+  readDB();
+
+  const body = await request.json();
+  const { roomName } = body;
+  if (DB.rooms.find((room) => room.roomName === roomName)) {
     return NextResponse.json(
       {
         ok: false,
-        message: `Room ${"replace this with room name"} already exists`,
+        message: `Room ${roomName} already exists`,
       },
       { status: 400 }
     );
-    }
-  
-    DB.enrollments.push({
-      studentId,
-      courseNo,
-    });
-    if(!foundRoom)
-    return NextResponse.json({
-      ok: true,
-      message: "You has enrolled a course successfully",
-    });
-  };
+  }
+
   const roomId = nanoid();
+  DB.rooms.push({
+    roomId,
+    roomName,
+  });
 
   //call writeDB after modifying Database
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    //roomId,
-    message: `Room ${"replace this with room name"} has been created`,
+    roomId,
+    message: `Room ${roomName} has been created`,
   });
+};
